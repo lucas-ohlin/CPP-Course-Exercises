75655058d2b570b4c7c6f87a9c33cf22156ba529
lucas-ohlin <lucas.persson.ohlin@hotmail.com>
2025-03-25 22:35:50 +0100

    Slight fix, now also includes the input number

--- exercise_3/main.cpp
+++ exercise_3/main.cpp
@@ -5,7 +5,7 @@ static int getUserInput() {
   int value;
   while (true) {
     std::cout << "Enter positive number: ";
-    if (std::cin >> value) {
+    if (std::cin >> value && value >= 0) {
       return value;
     } else {
       std::cout << "Invalid input. Please enter a number.\n";
@@ -17,17 +17,17 @@ static int getUserInput() {
 
 int main() {
   int num;
-  int total;
+  int total = 0;
   while (true) {
     num = getUserInput();
     std::cout << "Num: " << num << std::endl;
     if (num >= 0) {
-      for (int i = 0; i < num; i++) {
+      for (int i = 0; i <= num; i++) {
         if (i % 2 == 0) {
           total += i;
         }
       }
-      std::cout << "Total value of all even numbers from 0-" << num << " is: " << total;
+      std::cout << "Sum of all values of all even numbers from 0-" << num << " is: " << total;
       break;
     } else {
       std::cout << "Num needs to be positive...\n";
